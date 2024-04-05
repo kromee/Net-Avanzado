5d13610d8b795103789b0626f4301a594c9d07d5
Eduardo Dguez <renixedu@gmail.com>
2024-04-05 12:16:45 -0600

    EVENTOS
    Manejo de eventos

---
+++ Net-Avanzado/Net-Avanzado/Eventos.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace Net_Avanzado
+{
+	public class Eventos
+	{
+		public delegate void EjemploDelegado();
+		public event  EjemploDelegado? EjemploEvento;
+
+		public void Sumar(int a, int b) {
+			if (EjemploEvento != null)
+			{
+                EjemploEvento();
+                Console.WriteLine($"La suma es:{a + b} ");
+            }
+			else {
+                Console.WriteLine("No está suscrito al evento en la suma");
+            }
+		
+		}
+        public void Restar(int a, int b)
+        {
+            if (EjemploEvento != null)
+            {
+                EjemploEvento();
+                Console.WriteLine($"La resta es:{a - b} ");
+            }
+            else
+            {
+                Console.WriteLine("No está suscrito al evento en la resta ");
+            }
+           
+        }
+
+		
+    }
+
+    public class SuscriptorOperacionesVirtual{
+
+        private Eventos eventos;
+        private int A, B;
+
+        public void EjemploEventHandler() {
+            Console.WriteLine("La operación será ejecutada");
+        }
+
+        public SuscriptorOperacionesVirtual(int a, int b) {
+            eventos = new Eventos();
+
+            A = a;
+            B = b;
+
+            eventos.EjemploEvento += EjemploEventHandler;
+        }
+
+        public void OperacionSuma() {
+            eventos.Sumar(A, B);
+        }
+
+        public void OperacionResta() {
+            eventos.Restar(A, B);
+        }
+
+
+
+    }
+}
+
--- Net-Avanzado/Net-Avanzado/Program.cs
+++ Net-Avanzado/Net-Avanzado/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Net_Avanzado;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("DELEGADOS");
+
+
+/*Clase Delegados*/
 
 Delegados delegados = new Delegados();
 delegados.EjemploDelegado();
@@ -12,3 +15,10 @@ delegados.EjemploMetodoAnonimo();
 delegados.EjemploFunc();
 delegados.EjemploFun2();
 delegados.EjemploPredicate();
+
+Console.WriteLine("\n\n");
+
+Console.WriteLine("EVENTOS");
+SuscriptorOperacionesVirtual operaciones = new SuscriptorOperacionesVirtual(5, 6);
+operaciones.OperacionSuma();
+operaciones.OperacionResta();

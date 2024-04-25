82976052ce8f7efe62f4069bff748c78ca646fe6
Eduardo Dguez <renixedu@gmail.com>
2024-04-24 18:12:07 -0600

    Logs en la API
    ILoggerFactory, ILoggerProvider

---
+++ Log/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Log.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, ILoggerFactory factory, ILoggerProvider provider)
+    {
+        _logger = logger;
+        ILogger log = factory.CreateLogger("Log.Controllers.WeatherForecastController");
+        _logger = factory.CreateLogger<WeatherForecastController>();
+
+        _logger.LogError("Error ejemplo 1");
+        log.LogError("Ejemplo de error");
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
+
